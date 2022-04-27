5d6a125c9fd04a83da0b91db04acfe7ed599e491
dia-zx <dia_zx@mail.ru>
2022-04-28 01:12:42 +0300

    Lesson1 is done

---
+++ HomeWork/Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.ReadLine();
+        }
+    }
+}
